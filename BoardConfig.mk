a20a7b24c423288411ddd8345cf6157e2ab17c36
XiNGRZ <chenxingyu92@gmail.com>
2019-11-11 10:27:47 +0800

    nut: Drop sepolicy
    * Will be reworked in the future
    
    Change-Id: Idad6f28f6fb628744d478e37dbef84fd0df76895

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -156,8 +156,7 @@ TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
 
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += \
-    $(DEVICE_PATH)/sepolicy
+BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Shims
 TARGET_LD_SHIM_LIBS := \
